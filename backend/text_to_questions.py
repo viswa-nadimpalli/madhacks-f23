4f98ea0e2d3a55653858f88a63ebe5bc5cb393fd
Viswa Nadimpalli <viswanadim@gmail.com>
2023-11-12 01:31:25 -0600

    working cheatsheet.pdf generator

--- backend/text_to_questions.py
+++ backend/text_to_questions.py
@@ -3,7 +3,7 @@ from dotenv import load_dotenv
 load_dotenv()
 from openai import OpenAI
 from reportlab.lib.pagesizes import letter
-from reportlab.platypus import SimpleDocTemplate, Paragraph
+from reportlab.platypus import SimpleDocTemplate, Paragraph, Spacer
 from reportlab.lib.styles import getSampleStyleSheet
 import sys
 import os
@@ -13,18 +13,6 @@ client = OpenAI(api_key = os.getenv("OPENAI_API_KEY"))
 with open('output.txt', 'r', encoding='utf-8') as file:
     content = file.read()
 
-
-# Construct a prompt
-prompt = f"Generate questions and answers based on the following text:\n{content}\n"
-question_type = input("Enter the corresponding number of the type of question:\n1. True/False\n2. Multiple Choice\n3. Short Answer\nResponse: ")
-
-if question_type == "1":
-    prompt += "Make the questions 'True or False'. Fifty percent of the answers should be true, and fifty percent of the answers should be false."
-elif question_type == "2":
-    prompt += "Make the questions 'Multiple Choice'."
-elif question_type == "3":
-    prompt += "Make the questions 'Short Answer'."
-=======
 prompt = ""
 initial_question = input("Enter '1' for questions and '2' for a cheat sheet: ")
 
@@ -45,14 +33,27 @@ def text_to_pdf_styled(text, output_path):
     
     # Get sample styles
     styles = getSampleStyleSheet()
+    title_style = styles['Title']
     normal_style = styles['Normal']
 
-    # Build the story with paragraphs
+    # Build the story with title and paragraphs
     story = []
+
+    # Add title
+    title_text = "Jotter Cheat Sheet"
+    title = Paragraph(title_text, title_style)
+    story.append(title)
+    
+    # Add space after title
+    story.append(Spacer(1, 12))
+
+    # Add paragraphs with styles
     for paragraph in text.split('\n'):
         story.append(Paragraph(paragraph, normal_style))
+        # Add space between paragraphs
+        story.append(Spacer(1, 6))
     
-    # Add the story to the document
+    # Build the PDF
     doc.build(story)
 
 
@@ -86,10 +87,10 @@ if initial_question == "1":
         print("'Answers:' not found. Unexpecteed GPT-3 response")
         sys.exit(1)
 elif initial_question == "2":
-    prompt = f"Generate a cheat sheet based on the following text:\n{content}\n"
+    prompt = f"Generate a cheat sheet based on the following text:\n{content}\nDo not start the sheet with a 'Cheat Sheet' title"
     response_content = generate_responses(prompt).choices[0].message.content
     print(response_content)
-    text_to_pdf_styled(response_content, "od.pdf")
+    text_to_pdf_styled(response_content, "cheatsheet.pdf")
 else:
     print("Invalid Input")
     sys.exit(1)
