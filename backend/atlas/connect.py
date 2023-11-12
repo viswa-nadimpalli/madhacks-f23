4e0a10ff3ba9bbecc2ff5462eb226d2019f8c6df
Henry Olig <olighenry@gmail.com>
2023-11-11 23:23:19 -0600

    qf

--- backend/atlas/connect.py
+++ backend/atlas/connect.py
@@ -1,6 +1,5 @@
 from dotenv import load_dotenv
 from pymongo import MongoClient
-import connect
 load_dotenv()
 
 import sys
@@ -8,6 +7,6 @@ import os
 
 def getClient():
     mongodbkey = os.getenv("MONGODBKEY")
-    client = MongoClient('mongodb+srv://olig0007:8H1fapZ86Z5l7D7y@cluster0.cqo5ajt.mongodb.net/?retryWrites=true&w=majority')
+    client = MongoClient(mongodbkey)
     return client
 
